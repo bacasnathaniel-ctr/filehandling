7145b18b55abb403234db602af2d4cc9a3674133
bacasnathaniel-ctr <bacasnathaniel@gmail.com>
2026-04-19 17:54:49 +0800

    file_handling .py

--- file_handling .py
+++ file_handling .py
@@ -13,31 +13,31 @@ except Exception as e:
     print("Error:", e)
 
 
-# 2. Read File
+
 try:
     with open("notes.txt", "r") as file:
         content = file.read()
 
-    print("\n📄 Content of the file:")
+    print("\n Content of the file:")
     print(content)
 
 except FileNotFoundError:
-    print("❌ File not found.")
+    print(" File not found.")
 
 
-# 3. Append new data
+
 try:
     new_note = input("\nEnter another note: ")
 
-    # Append using "a" mode
+    
     with open("notes.txt", "a") as file:
         file.write("\n" + new_note)
 
-    # Display updated content
+    
     with open("notes.txt", "r") as file:
         updated_content = file.read()
 
-    print("\n📄 Updated content:")
+    print("\n Updated content:")
     print(updated_content)
 
 except Exception as e:
